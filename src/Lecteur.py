3b551ee35f28dead251f21f6dc2ceeee070225b6
Nicolas Anjorand <nicolasanjorand@MacBook-Air-de-Nicolas.local>
2024-02-07 16:27:25 +0100

    clean classes

--- src/Lecteur.py
+++ src/Lecteur.py
@@ -1,9 +1,3 @@
 class Lecteur:
     def __init__(self, badge_detecte=False):
         self.badge_detecte = badge_detecte
-
-    def detecter_badge(self):
-        self.badge_detecte = True
-
-    def reset_badge(self):
-        self.badge_detecte = False
--- src/MoteurOuverture.py
+++ src/MoteurOuverture.py
@@ -4,7 +4,3 @@ from .Porte import Porte
 class MoteurOuverture:
     def __init__(self, porte: Porte):
         self._porte = porte
-
-    def interroger(self, lecteur: Lecteur):
-        if lecteur.badge_detecte:
-            self._porte.ouvrir()
