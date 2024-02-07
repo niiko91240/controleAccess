f1168346f2a13dc5d6a07f27e77b3751dc62d87b
Nicolas Anjorand <nicolasanjorand@MacBook-Air-de-Nicolas.local>
2024-02-07 16:47:41 +0100

    test2-ok

--- tests/test_lecteur.py
+++ tests/test_lecteur.py
@@ -4,10 +4,15 @@ from src.Lecteur import Lecteur
 
 class TestMain(unittest.TestCase):
 
-    def test_hello_morning(self):
+    def test_detecter_badge(self):
         lecteur = Lecteur()
         lecteur.detecter_badge()
         self.assertTrue(lecteur.badge_detecte)
+
+    def test_detecter_badge_wrong(self):
+        lecteur = Lecteur()
+        self.assertFalse(lecteur.badge_detecte)
+
 
 
 
